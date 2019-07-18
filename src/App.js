839ed114f6632d49a4dfd24b73e1ea9abc88f7dd
Jalen Parham <jalenparham97@gmail.com>
2019-07-18 12:09:45 -0400

    uppdating app with shop page, colitem comp, and colprev comp

--- src/App.js
+++ src/App.js
@@ -1,12 +1,17 @@
 import React from 'react'
+import { Switch, Route } from 'react-router-dom'
 import '../src/App.css'
 
 import Homepage from './pages/homepage/Homepage'
+import Shoppage from './pages/shoppage/Shoppage'
 
 function App() {
   return (
     <div className="App">
-      <Homepage />
+      <Switch>
+        <Route exact path="/" component={Homepage} />
+        <Route exact path="/shop" component={Shoppage} />
+      </Switch>
     </div>
   )
 }
